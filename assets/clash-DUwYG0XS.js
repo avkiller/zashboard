187a99b307617d577daa22e0a8ef2dff870a8cfe
avkiller <avkiller@users.noreply.github.com>
2026-08-24 10:44:00 +0000

    deploy: d03a3eb0c9bb5f92a085bdfe87344d63e7456ae4

---
+++ assets/clash-DUwYG0XS.js
@@ -0,0 +1,1 @@
+import{T as e,X as t,Y as n}from"./index-3tA_KGBW.js";var r=async()=>{e.value=(await n()).data},i=async e=>{await t(e),r()};export{r as fetchConfigs,i as updateConfigs};
---
+++ assets/singbox-Boo9OVGq.js
@@ -0,0 +1,1 @@
+import{h as e,m as t}from"./index-3tA_KGBW.js";var n=async()=>{e.value=[],t.value=[]};export{n as fetchRules};
